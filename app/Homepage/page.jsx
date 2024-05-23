dde30d6ff5d78a732995869b17c80168f5828f9c
Axel <johnaxelcortez21@gmail.com>
2024-05-23 18:29:48 +1000

    debug v2

--- app/Homepage/page.jsx
+++ app/Homepage/page.jsx
@@ -1,30 +1,32 @@
 "use client";
 import React, { useEffect } from "react";
+import { Amplify } from "aws-amplify";
 import Sidebar from "./components/sidebar";
 import Content from "./components/content";
-import { useSetAtom } from "jotai";
-import { registerUser } from "./datastore";
+// import config from "../../src/amplifyconfiguration.json";
+// Amplify.configure(config);
 
 const Homepage = ({ user }) => {
-  const registerU = useSetAtom(registerUser);
+  // const registerU = useSetAtom(registerUser);
+  // console.log(user);
+  // useEffect(() => {
+  //   const handleSignin = async () => {
+  //     if (user) {
+  //       try {
+  //         const result = await registerU();
+  //         if (result.success) {
+  //           console.log("User registered successfully");
+  //         } else {
+  //           console.error("User registration failed:", result.message);
+  //         }
+  //       } catch (error) {
+  //         console.error("Error registering user:", error);
+  //       }
+  //     }
+  //   };
+  //   handleSignin();
+  // }, [user]);
   console.log(user);
-  useEffect(() => {
-    const handleSignin = async () => {
-      if (user) {
-        try {
-          const result = await registerU();
-          if (result.success) {
-            console.log("User registered successfully");
-          } else {
-            console.error("User registration failed:", result.message);
-          }
-        } catch (error) {
-          console.error("Error registering user:", error);
-        }
-      }
-    };
-    handleSignin();
-  }, [user]);
   return (
     <div className='flex flex-row w-full h-screen max-h-screen'>
       <Sidebar />
--- app/page.jsx
+++ app/page.jsx
@@ -4,12 +4,9 @@ import { Amplify } from "aws-amplify";
 import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 import config from "../src/amplifyconfiguration.json";
-
 Amplify.configure(config);
 
 export default function Home() {
-
-
   return (
     <Authenticator>
       {({ signOut, user }) => {
